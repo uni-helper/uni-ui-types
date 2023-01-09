06af89e69da4d494b245f90bc7372ed7ab803b46
ModyQyW <wurui-dev@foxmail.com>
2023-01-09 10:44:50 +0800

    chore: update

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
   '*.md': 'markdownlint --fix',
-  '*.{js,cjs,mjs,ts,cts,mts}':
+  './types/*.{js,cjs,mjs,ts,cts,mts}':
     'eslint --fix --cache --no-error-on-unmatched-pattern --ignore-path=.gitignore',
 };
