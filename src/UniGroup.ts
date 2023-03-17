3e6037afa9848ba2aeb1019caf182218bc3fe9f8
ModyQyW <wurui-dev@foxmail.com>
2023-03-17 14:16:45 +0800

    feat: update UniGroup

--- src/UniGroup.ts
+++ src/UniGroup.ts
@@ -9,12 +9,23 @@ import { Component } from '@uni-helper/uni-app-types';
  */
 type _UniGroupMode = 'default' | 'card';
 
+/** 点击时触发 */
+interface _UniGroupOnClick {
+  (): void;
+}
+
 /** 分组属性 */
 type _UniGroupProps = Partial<{
   /** 主标题 */
   title: string;
-  /** 分组间隔 */
-  top: number;
+  /**
+   * 分组间隔
+   *
+   * 单位为 px
+   *
+   * 默认为 10
+   */
+  top: number | string;
   /**
    * 模式
    *
@@ -25,6 +36,14 @@ type _UniGroupProps = Partial<{
    * 默认为 default
    */
   mode: _UniGroupMode;
+  /**
+   * 是否开启统计
+   *
+   * 默认为 false
+   */
+  stat: boolean;
+  /** 点击时触发 */
+  onClick: _UniGroupOnClick;
 }>;
 
 /** 分组组件可用于将组件分组，添加间隔，以产生明显的区块 */
@@ -35,6 +54,7 @@ type _UniGroupInstance = InstanceType<_UniGroup>;
 
 export {
   _UniGroupMode as UniGroupMode,
+  _UniGroupOnClick as UniGroupOnClick,
   _UniGroupProps as UniGroupProps,
   _UniGroup as UniGroup,
   _UniGroupInstance as UniGroupInstance,
@@ -50,6 +70,7 @@ declare global {
      * card 卡片
      */
     export type UniGroupMode = _UniGroupMode;
+    export interface UniGroupOnClick extends _UniGroupOnClick {}
     /** 分组属性 */
     export type UniGroupProps = _UniGroupProps;
     /** 分组组件可用于将组件分组，添加间隔，以产生明显的区块 */
--- tests/UniGroup.test-d.ts
+++ tests/UniGroup.test-d.ts
@@ -1,9 +1,18 @@
 import { describe, expectTypeOf } from 'vitest';
-import type { UniGroupMode, UniGroupProps, UniGroup, UniGroupInstance } from '@/UniGroup';
+import type {
+  UniGroupMode,
+  UniGroupOnClick,
+  UniGroupProps,
+  UniGroup,
+  UniGroupInstance,
+} from '@/UniGroup';
 
 describe('UniGroup', () => {
   expectTypeOf<UniGroupMode>().toBeString();
   expectTypeOf<UniGroupMode>().toEqualTypeOf<UniHelper.UniGroupMode>();
+
+  expectTypeOf<UniGroupOnClick>().toBeFunction();
+  expectTypeOf<UniGroupOnClick>().toEqualTypeOf<UniHelper.UniGroupOnClick>();
 
   expectTypeOf<UniGroupProps>().toBeObject();
   expectTypeOf<UniGroupProps>().toEqualTypeOf<UniHelper.UniGroupProps>();
